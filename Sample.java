ada4e2aae6b1e1f6fb13e6751181a655aafecdcf
dayanandh <50669970+dayanandh@users.noreply.github.com>
2019-05-23 14:52:46 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,17 @@
+	package myPack;
+	class Sample
+	{
+		private int number;
+		
+		public Sample(int number)
+		{
+
+			this.number=number;
+		}
+
+		public int getNumber()
+		{
+
+			return this.number;
+		}
+	}
